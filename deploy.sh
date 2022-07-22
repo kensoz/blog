1076337d5451560abbf24930031535432a241396
kensoz <kensozlian@gmail.com>
2022-07-22 13:46:34 +0900

    test

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ cd docs/.vuepress/dist
 git init
 git add -A
 git commit -m 'deploy'
-
+#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
 git push -f git@study.github.com:kensoz/blog.git master:gh-pages
 
 cd -
